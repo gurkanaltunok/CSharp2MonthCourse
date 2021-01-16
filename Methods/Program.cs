9ed90fc4eaa1fca5bd758b7ae2ac1b1af86bd2b1
Oğuz Gürkan Altunok <oguzgurkan2001@gmail.com>
2021-01-16 22:43:24 +0300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 50;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2};
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("=================");
+            }
+
+            Console.WriteLine("------------------Metotlar------------------");
+
+            //instance
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+//Don't repeat yourself - DRY - Clean Code
